d73a9580a168f251be50bbfd77fc8d34a95317d7
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2018-07-18 16:09:15 +0200

    Initial working version of the project

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+/* Path to pdf files */
+$path = 'pdf_files';
+
+/* Filter pdf files */
+$files = preg_grep('~\.(pdf)$~', scandir($path));
+
+/* Set column headers */
+$headers = [
+  1 => "ID",
+  2 => "File name",
+  3 => "Action",
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PDF explorer</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <script src="/js/script.js"></script>
+</head>
+<body>
+  <div class="page">
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="index.php">
+        <img src="/images/pdf.png" width="100" height="100" class="d-inline-block align-top" alt="">
+      </a>
+    </nav>
+    <div class="container mt-3">
+      <input type="text" id="searchInput" onkeyup="filterData()" placeholder="Search for files ....">
+      <table id="fileTable" class="table table-hover">
+        <thead>
+          <tr>
+          <?php foreach ($headers as $header): ?>
+            <th><?php echo $header; ?></th>
+          <?php endforeach; ?>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <?php foreach ($files as $key => $value): ?>
+            <td><?php echo $key; ?></td>
+            <td><?php echo $value; ?></td>
+            <td> <a  class="btn btn-danger" href="<?php echo $path."/".$value ?>" target="_blank">Preview</a> </td>
+          </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div id="loading"></div>
+  <footer class="page-footer font-small blue">
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://github.com/miroslavvidovic"> miroslavvidovic<a>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Filter table data based on user input
+function filterData() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("fileTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('table')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 2000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('.page', true);
+  setVisible('#loading', false);
+});
